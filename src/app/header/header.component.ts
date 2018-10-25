6a05a9fea3f195a6773ad6bc26e8b225b741c376
sanjanashetty <sanjanashetty888@gmail.com>
2018-10-25 20:39:52 +0530

    home content edot

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +10,9 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
+
+
 
 }
